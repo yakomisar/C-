8e215d38940e2aa0f9e1b7d53a73198568843c40
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2022-02-28 20:43:37 +0300

    added ex03

---
+++ day04/ex03/Cure.hpp
@@ -0,0 +1,19 @@
+#ifndef CURE_HPP
+#define CURE_HPP
+
+#include "AMateria.hpp"
+
+class Cure : public AMateria
+{
+private:
+	/* data */
+public:
+	Cure();
+	Cure(Cure const &other);
+	Cure &operator=(Cure const &other);
+	~Cure();
+	AMateria *clone() const;
+	void use(ICharacter &target);
+};
+
+#endif
